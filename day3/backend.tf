78463e59b5df8bd320b53c6dfa362f9edb2ab8e6
santoshkundula <149155836+santoshkundula@users.noreply.github.com>
2023-12-10 06:18:33 +0000

    Revert "pull"

--- day3/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "ts-rg-dev-netflix-4656"
-    storage_account_name = "tsstdevnetflix4656"
-    container_name       = "tfnetflix"
-    key                  = "dev.terraform.tfstate"
-  }
-}
